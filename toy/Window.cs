b2066b18e3fe15c451cb556fd5a8cb95b9c315d6
BlueSeaGameStudio <love_1986915@163.com>
2022-04-22 23:20:31 +0800

    Signed-off-by: BlueSeaGameStudio <love_1986915@163.com>

---
+++ toy/Window.cs
@@ -0,0 +1,56 @@
+﻿using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+
+namespace LearnOpenTK {
+    public class Window : GameWindow {
+        private int vao;
+        private Shader shader;
+
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+	    : base(gameWindowSettings, nativeWindowSettings) { }
+
+        protected override void OnLoad() {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            var vbo = GL.GenBuffer();
+            var vertices = new float[] { 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+	    vao = GL.GenVertexArray();
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            shader.UseProgram();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e) {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            shader.UseProgram();
+            GL.BindVertexArray(vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e) {
+            base.OnUpdateFrame(e);
+            if (KeyboardState.IsKeyDown(Keys.Escape)) {
+                Close();
+            }
+        }
+
+        protected override void OnResize(ResizeEventArgs e) {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+    }
+}
