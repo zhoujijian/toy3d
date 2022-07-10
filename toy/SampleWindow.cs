76342742fcbb61d4b9e030a35fe481a9466341c8
BlueSeaGameStudio <love_1986915@163.com>
2022-07-10 14:26:59 +0800

    multiple light

--- toy/SampleWindow.cs
+++ toy/SampleWindow.cs
@@ -18,7 +18,8 @@ namespace Toy3d.Samples {
         private float wheelOffsetY;
         private bool firstWheel = true;
 
-        private Light light;
+        // private Light light;
+        private Light[] pointLights;
         private int textureIdDiffuse;
         private int textureIdSpecular;
 
@@ -108,7 +109,14 @@ namespace Toy3d.Samples {
             camera.Yaw = YAW;
             camera.Pitch = PITCH;
 
-            light = new Light(new Vector3(0.0f, 1.0f, -1.0f));
+            // light = new Light(new Vector3(0.0f, 1.0f, -1.0f));
+            
+            pointLights = new Light[] {
+                new Light(new Vector3( 0.7f,  0.2f,  2.0f)),
+                new Light(new Vector3( 2.3f, -3.3f, -4.0f)),
+                new Light(new Vector3(-4.0f,  2.0f, -12.0f)),
+                new Light(new Vector3( 0.0f,  0.0f, -3.0f))
+            };
 
             textureIdDiffuse = ImageTexture.LoadFromFile("Images/container2.png").Handle;
             textureIdSpecular = ImageTexture.LoadFromFile("Images/container2_specular.png").Handle;
@@ -131,25 +139,68 @@ namespace Toy3d.Samples {
             GL.UniformMatrix4(GL.GetUniformLocation(shader.ProgramId, "uView"), false, ref view);
             GL.UniformMatrix4(GL.GetUniformLocation(shader.ProgramId, "uProjection"), false, ref projection);
             GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "viewWorldPosition"), camera.Position.X, camera.Position.Y, camera.Position.Z);
-            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "material.shininess"), 32.0f);
-            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "light.ambient"), 0.0f, 0.0f, 0.0f);
-            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "light.diffuse"), 0.5f, 0.5f, 0.5f);
-            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "light.specular"), 1.0f, 1.0f, 1.0f);
-            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "light.position"), light.LocalPosition.X, light.LocalPosition.Y, light.LocalPosition.Z);
 
+            // direction light
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "directionLight.direction"), -0.2f, -0.1f, -0.3f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "directionLight.ambient"), 0.05f, 0.05f, 0.05f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "directionLight.diffuse"), 0.4f, 0.4f, 0.4f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "directionLight.specular"), 0.5f, 0.5f, 0.5f);
+
+            // point light
+            var p0 = pointLights[0].LocalPosition;
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[0].position"), p0.X, p0.Y, p0.Z);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[0].ambient"), 0.05f, 0.05f, 0.05f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[0].diffuse"), 0.8f, 0.8f, 0.8f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[0].specular"), 1.0f, 1.0f, 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[0].constant"), 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[0].linear"), 0.09f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[0].quadratic"), 0.032f);
+
+            var p1 = pointLights[1].LocalPosition;
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[1].position"), p1.X, p1.Y, p1.Z);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[1].ambient"), 0.05f, 0.05f, 0.05f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[1].diffuse"), 0.8f, 0.8f, 0.8f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[1].specular"), 1.0f, 1.0f, 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[1].constant"), 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[1].linear"), 0.09f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[1].quadratic"), 0.032f);
+
+            var p2 = pointLights[1].LocalPosition;
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[2].position"), p2.X, p2.Y, p2.Z);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[2].ambient"), 0.05f, 0.05f, 0.05f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[2].diffuse"), 0.8f, 0.8f, 0.8f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[2].specular"), 1.0f, 1.0f, 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[2].constant"), 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[2].linear"), 0.09f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[2].quadratic"), 0.032f);
+
+            var p3 = pointLights[1].LocalPosition;
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[3].position"), p3.X, p3.Y, p3.Z);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[3].ambient"), 0.05f, 0.05f, 0.05f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[3].diffuse"), 0.8f, 0.8f, 0.8f);
+            GL.Uniform3(GL.GetUniformLocation(shader.ProgramId, "pointLights[3].specular"), 1.0f, 1.0f, 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[3].constant"), 1.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[3].linear"), 0.09f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "pointLights[3].quadratic"), 0.032f);
+
+            // material
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "material.shininess"), 32.0f);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "material.diffuse"), 0);
+            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "material.specular"), 1);
+            
             GL.ActiveTexture(TextureUnit.Texture0);
             GL.BindTexture(TextureTarget.Texture2D, textureIdDiffuse);
-            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "material.diffuse"), 0);
-
             GL.ActiveTexture(TextureUnit.Texture1);
             GL.BindTexture(TextureTarget.Texture2D, textureIdSpecular);
-            GL.Uniform1(GL.GetUniformLocation(shader.ProgramId, "material.specular"), 1);
 
             // GL.DrawElements(PrimitiveType.Triangles, 3, DrawElementsType.UnsignedInt, 0);
             GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
             GL.BindVertexArray(0);
 
-            light.Draw(camera);
+            // light.Draw(camera);
+            foreach (var light in pointLights) {
+                light.Draw(camera);
+            }
 
             SwapBuffers();
         }
--- toy/Shaders/shader.frag
+++ toy/Shaders/shader.frag
@@ -6,35 +6,76 @@ struct Material {
     float shininess;
 };
 
-struct Light {
-    vec3 position;
+struct DirectionLight {
+    vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
 };
 
+struct PointLight {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+#define NR_POINT_LIGHTS 4
+
 out vec4 outputColor;
 
 uniform vec3 viewWorldPosition;
 uniform Material material;
-uniform Light light;
+uniform DirectionLight directionLight;
+uniform PointLight pointLights[NR_POINT_LIGHTS];
 
 in vec3 fragWorldNormal;
 in vec3 fragWorldPosition;
 in vec2 fragTextureCoord;
 
-void main()
-{
+vec3 CalculateDirectionLight(DirectionLight light) {
     vec3 ambient = light.ambient * texture(material.diffuse, fragTextureCoord).rgb;
-    
-    vec3 lightDirection = normalize(light.position - fragWorldPosition);
+
+    vec3 lightDirection = normalize(-light.direction);
     float factorDiffuse = max(dot(lightDirection, normalize(fragWorldNormal)), 0.0);
-    vec3 diffuse = factorDiffuse * light.diffuse * vec3(texture(material.diffuse, fragTextureCoord));
+    vec3 diffuse = factorDiffuse * light.diffuse * texture(material.diffuse, fragTextureCoord).rgb;
 
     vec3 viewDirection = normalize(viewWorldPosition - fragWorldPosition);
     vec3 reflectLightDirection = reflect(-lightDirection, fragWorldNormal);
     float factorSpecular = pow(max(dot(viewDirection, reflectLightDirection), 0.0), material.shininess);
-    vec3 specular = factorSpecular * light.specular * vec3(texture(material.specular, fragTextureCoord));
+    vec3 specular = factorSpecular * light.specular * texture(material.specular, fragTextureCoord).rgb;
 
-    outputColor = vec4(ambient + diffuse + specular, 1.0);
+    return ambient + diffuse + specular;
+}
+
+vec3 CalculatePointLight(PointLight light) {
+    vec3 ambient = light.ambient * texture(material.diffuse, fragTextureCoord).rgb;
+    
+    vec3 lightDirection = normalize(light.position - fragWorldPosition);
+    float factorDiffuse = max(dot(lightDirection, normalize(fragWorldNormal)), 0.0);
+    vec3 diffuse = factorDiffuse * light.diffuse * texture(material.diffuse, fragTextureCoord).rgb;
+
+    vec3 viewDirection = normalize(viewWorldPosition - fragWorldPosition);
+    vec3 reflectLightDirection = reflect(-lightDirection, fragWorldNormal);
+    float factorSpecular = pow(max(dot(viewDirection, reflectLightDirection), 0.0), material.shininess);
+    vec3 specular = factorSpecular * light.specular * texture(material.specular, fragTextureCoord).rgb;
+
+    float d = length(light.position - fragWorldPosition);
+    float attenuation = 1.0 / (light.constant + light.linear * d + light.quadratic * (d * d));
+    ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
+
+    return ambient + diffuse + specular;
+}
+
+void main() {
+    vec3 color = CalculateDirectionLight(directionLight);
+    for (int i = 0; i < NR_POINT_LIGHTS; i++) {
+        color += CalculatePointLight(pointLights[i]);
+    }
+    outputColor = vec4(color, 1.0);
 }
